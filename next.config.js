fa7f619ddccd81e7e2cfadc70250166ea8b21e27
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2021-08-29 20:08:12 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  images: {
+    domains: [
+      "cdn0.iconfinder.com",
+      "platform-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com",
+      "1000logos.net",
+      "pbs.twimg.com",
+      "upload.wikimedia.org",
+      "png.pngtree.com",
+      "avatars0.githubusercontent.com",
+      "loogart.com",
+      "media-exp1.licdn.com",
+      "mir-s3-cdn-cf.behance.net",
+      "images-na.ssl-images-amazon.com",
+      "assets.telegraphindia.com",
+      "www.biography.com",
+      "thumbor.forbes.com",
+      "api.time.com",
+    ],
+  },
+};
