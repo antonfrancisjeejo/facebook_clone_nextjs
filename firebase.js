c9214e0d1c2d30907cd34d7891964d85b9561541
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2021-12-13 03:10:23 +0530

    New firebase config

--- firebase.js
+++ firebase.js
@@ -3,14 +3,13 @@ import "firebase/storage";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARNrydlfqu_UtuFjrSdCvsCkHuAnKtnMQ",
-  authDomain: "facebook-nextjs-clone-a1b5d.firebaseapp.com",
-  projectId: "facebook-nextjs-clone-a1b5d",
-  storageBucket: "facebook-nextjs-clone-a1b5d.appspot.com",
-  messagingSenderId: "384169591723",
-  appId: "1:384169591723:web:85a4c1561915b7099e88ee",
+  apiKey: "AIzaSyBd1V6iGGKbZUojrxbpoQY-WL-E2PXJJWk",
+  authDomain: "facebook-clone-nextjs-5b0f7.firebaseapp.com",
+  projectId: "facebook-clone-nextjs-5b0f7",
+  storageBucket: "facebook-clone-nextjs-5b0f7.appspot.com",
+  messagingSenderId: "942093862639",
+  appId: "1:942093862639:web:6af3529d9341a575247d1a",
 };
-
 const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
   : firebase.app();
